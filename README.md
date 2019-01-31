4d744a41387f63b3d4f3fcca1e1df67dfda524c7
Anshuman Suri <anshuman14021@iiitd.ac.in>
2019-01-31 01:55:17 -0800

    Post competition push

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# semeval
+# EmoContext : A Shared Task on Contextual Emotion Detection in Text
+
+My submission to the 3rd SemEval-2019 task : EmoContext.
+
+### Notebook includes:
+
+* Regex-based replacement for slangs/typos
+* Basic data processing to increase vocabulary coverage
+* Recurrent model with LSTM and GRU bidirectional cells (skip connections), attention and cyclic-LR
+
+### Coming Soon:
+
+* emoji2vec for handling emojis in data
---
+++ utils.py
@@ -0,0 +1,415 @@
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
+
+import keras
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model, Model
+from keras.layers import concatenate, Activation, GlobalAveragePooling1D, GlobalMaxPooling1D, Layer, Dense, Embedding, LSTM, GRU, Dropout, SpatialDropout1D, Input, Average, Bidirectional, BatchNormalization
+from keras.callbacks import Callback
+from keras import initializers, regularizers, constraints, optimizers, layers
+
+import emoji
+import re
+
+import json, argparse, os
+import re
+import io
+import sys
+
+print("Loading utils module")
+
+label2emotion = {0:"others", 1:"happy", 2: "sad", 3:"angry"}
+emotion2label = {"others":0, "happy":1, "sad":2, "angry":3}
+
+def get_sswe_embeddings(wordIndex):
+    oov = []
+    count, total = 0, 0
+    embeddingMatrix = np.zeros((len(wordIndex) + 1, 150))
+    embeddingsIndex = {}
+    
+    def get_start_index(lis):
+        for i, j in enumerate(lis):
+            try:
+                _ = float(j)
+                return i
+            except:
+                continue
+
+    faulty_count = 0
+    with open('sswe.tsv', 'r', errors='ignore') as f:
+        for i, line in enumerate(f):
+            stuff = line.rstrip('\n').split('\t')
+            breakoff_point = get_start_index(stuff)
+            word = "".join(stuff[:breakoff_point])
+            try:
+                embeddingsIndex[word] = np.asarray(stuff[breakoff_point:breakoff_point + 150], dtype='float32')
+            except:
+                faulty_count += 1
+                continue
+    print('Found unreadable %d word vectors' % faulty_count)
+    print('Found %s word vectors.' % len(embeddingsIndex))
+    for word, i in wordIndex.items():
+        embeddingVector = embeddingsIndex.get(word)
+        total += 1
+        if embeddingVector is not None:
+            # words not found in embedding index will be all-zeros.
+            embeddingMatrix[i] = embeddingVector
+            count += 1
+        else:
+            oov.append(word)
+    print("Found embedding for", str((100 * count) / total), "% embeddings")
+    return embeddingMatrix, oov
+    
+
+def split_into_three(texts, tknzr):
+    middle, left, right = [], [], []
+    for text in texts:
+        l, m, r = text.split(' <eos> ')
+        middle.append(m)
+        left.append(l)
+        right.append(r)
+    tokenize = lambda x: tknzr.texts_to_sequences(x)
+    return (tokenize(left), tokenize(middle), tokenize(right))
+
+
+def getEmbeddingMatrix(wordIndex, EMBEDDING_DIM):
+    """Populate an embedding matrix using a word-index. If the word "happy" has an index 19,
+       the 19th row in the embedding matrix should contain the embedding vector for the word "happy".
+    Input:
+        wordIndex : A dictionary of (word : index) pairs, extracted using a tokeniser
+    Output:
+        embeddingMatrix : A matrix where every row has 100 dimensional GloVe embedding
+    """
+    embeddingsIndex = {}
+    # Load the embedding vectors from ther GloVe file
+    with io.open(os.path.join('./glove.6B.%dd.txt' % EMBEDDING_DIM), encoding="utf8") as f:
+        for line in f:
+            values = line.split()
+            word = values[0]
+            embeddingVector = np.asarray(values[1:], dtype='float32')
+            embeddingsIndex[word] = embeddingVector
+    
+    print('Found %s word vectors.' % len(embeddingsIndex))
+    
+    oov = []
+    # Minimum word index of any word is 1. 
+    embeddingMatrix = np.zeros((len(wordIndex) + 1, EMBEDDING_DIM))
+    count, total= 0, 0
+    for word, i in wordIndex.items():
+        embeddingVector = embeddingsIndex.get(word)
+        total += 1
+        if embeddingVector is not None:
+            # words not found in embedding index will be all-zeros.
+            embeddingMatrix[i] = embeddingVector
+            count += 1
+        else:
+            oov.append(word)
+    
+    print("Found embedding for", str((100 * count) / total), "% embeddings")
+    return embeddingMatrix, oov
+
+
+# https://www.kaggle.com/hireme/fun-api-keras-f1-metric-cyclical-learning-rate/code
+
+class CyclicLR(Callback):
+    """This callback implements a cyclical learning rate policy (CLR).
+    The method cycles the learning rate between two boundaries with
+    some constant frequency, as detailed in this paper (https://arxiv.org/abs/1506.01186).
+    The amplitude of the cycle can be scaled on a per-iteration or 
+    per-cycle basis.
+    This class has three built-in policies, as put forth in the paper.
+    "triangular":
+        A basic triangular cycle w/ no amplitude scaling.
+    "triangular2":
+        A basic triangular cycle that scales initial amplitude by half each cycle.
+    "exp_range":
+        A cycle that scales initial amplitude by gamma**(cycle iterations) at each 
+        cycle iteration.
+    For more detail, please see paper.
+    
+    # Example
+        ```python
+            clr = CyclicLR(base_lr=0.001, max_lr=0.006,
+                                step_size=2000., mode='triangular')
+            model.fit(X_train, Y_train, callbacks=[clr])
+        ```
+    
+    Class also supports custom scaling functions:
+        ```python
+            clr_fn = lambda x: 0.5*(1+np.sin(x*np.pi/2.))
+            clr = CyclicLR(base_lr=0.001, max_lr=0.006,
+                                step_size=2000., scale_fn=clr_fn,
+                                scale_mode='cycle')
+            model.fit(X_train, Y_train, callbacks=[clr])
+        ```    
+    # Arguments
+        base_lr: initial learning rate which is the
+            lower boundary in the cycle.
+        max_lr: upper boundary in the cycle. Functionally,
+            it defines the cycle amplitude (max_lr - base_lr).
+            The lr at any cycle is the sum of base_lr
+            and some scaling of the amplitude; therefore 
+            max_lr may not actually be reached depending on
+            scaling function.
+        step_size: number of training iterations per
+            half cycle. Authors suggest setting step_size
+            2-8 x training iterations in epoch.
+        mode: one of {triangular, triangular2, exp_range}.
+            Default 'triangular'.
+            Values correspond to policies detailed above.
+            If scale_fn is not None, this argument is ignored.
+        gamma: constant in 'exp_range' scaling function:
+            gamma**(cycle iterations)
+        scale_fn: Custom scaling policy defined by a single
+            argument lambda function, where 
+            0 <= scale_fn(x) <= 1 for all x >= 0.
+            mode paramater is ignored 
+        scale_mode: {'cycle', 'iterations'}.
+            Defines whether scale_fn is evaluated on 
+            cycle number or cycle iterations (training
+            iterations since start of cycle). Default is 'cycle'.
+    """
+
+    def __init__(self, base_lr=0.001, max_lr=0.006, step_size=2000., mode='triangular',
+                 gamma=1., scale_fn=None, scale_mode='cycle'):
+        super(CyclicLR, self).__init__()
+
+        self.base_lr = base_lr
+        self.max_lr = max_lr
+        self.step_size = step_size
+        self.mode = mode
+        self.gamma = gamma
+        if scale_fn == None:
+            if self.mode == 'triangular':
+                self.scale_fn = lambda x: 1.
+                self.scale_mode = 'cycle'
+            elif self.mode == 'triangular2':
+                self.scale_fn = lambda x: 1/(2.**(x-1))
+                self.scale_mode = 'cycle'
+            elif self.mode == 'exp_range':
+                self.scale_fn = lambda x: gamma**(x)
+                self.scale_mode = 'iterations'
+        else:
+            self.scale_fn = scale_fn
+            self.scale_mode = scale_mode
+        self.clr_iterations = 0.
+        self.trn_iterations = 0.
+        self.history = {}
+
+        self._reset()
+
+    def _reset(self, new_base_lr=None, new_max_lr=None,
+               new_step_size=None):
+        """Resets cycle iterations.
+        Optional boundary/step size adjustment.
+        """
+        if new_base_lr != None:
+            self.base_lr = new_base_lr
+        if new_max_lr != None:
+            self.max_lr = new_max_lr
+        if new_step_size != None:
+            self.step_size = new_step_size
+        self.clr_iterations = 0.
+        
+    def clr(self):
+        cycle = np.floor(1+self.clr_iterations/(2*self.step_size))
+        x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)
+        if self.scale_mode == 'cycle':
+            return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0, (1-x))*self.scale_fn(cycle)
+        else:
+            return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0, (1-x))*self.scale_fn(self.clr_iterations)
+        
+    def on_train_begin(self, logs={}):
+        logs = logs or {}
+
+        if self.clr_iterations == 0:
+            K.set_value(self.model.optimizer.lr, self.base_lr)
+        else:
+            K.set_value(self.model.optimizer.lr, self.clr())        
+            
+    def on_batch_end(self, epoch, logs=None):
+        
+        logs = logs or {}
+        self.trn_iterations += 1
+        self.clr_iterations += 1
+
+        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
+        self.history.setdefault('iterations', []).append(self.trn_iterations)
+
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+        
+        K.set_value(self.model.optimizer.lr, self.clr())
+
+
+def getMetrics(predictions, ground):
+    discretePredictions = to_categorical(predictions.argmax(axis=1), 4)
+    
+    truePositives = np.sum(discretePredictions*ground, axis=0)
+    falsePositives = np.sum(np.clip(discretePredictions - ground, 0, 1), axis=0)
+    falseNegatives = np.sum(np.clip(ground-discretePredictions, 0, 1), axis=0)
+    
+    print("True Positives per class : ", truePositives)
+    print("False Positives per class : ", falsePositives)
+    print("False Negatives per class : ", falseNegatives)
+    
+    # ------------- Macro level calculation ---------------
+    macroPrecision = 0
+    macroRecall = 0
+    # We ignore the "Others" class during the calculation of Precision, Recall and F1
+    for c in range(1, 4):
+        precision = truePositives[c] / (truePositives[c] + falsePositives[c])
+        macroPrecision += precision
+        recall = truePositives[c] / (truePositives[c] + falseNegatives[c])
+        macroRecall += recall
+        f1 = ( 2 * recall * precision ) / (precision + recall) if (precision+recall) > 0 else 0
+        print("Class %s : Precision : %.3f, Recall : %.3f, F1 : %.3f" % (label2emotion[c], precision, recall, f1))
+    
+    macroPrecision /= 3
+    macroRecall /= 3
+    macroF1 = (2 * macroRecall * macroPrecision ) / (macroPrecision + macroRecall) if (macroPrecision+macroRecall) > 0 else 0
+    print("Ignoring the Others class, Macro Precision : %.4f, Macro Recall : %.4f, Macro F1 : %.4f" % (macroPrecision, macroRecall, macroF1))   
+    
+    # ------------- Micro level calculation ---------------
+    truePositives = truePositives[1:].sum()
+    falsePositives = falsePositives[1:].sum()
+    falseNegatives = falseNegatives[1:].sum()    
+    
+    print("Ignoring the Others class, Micro TP : %d, FP : %d, FN : %d" % (truePositives, falsePositives, falseNegatives))
+    
+    microPrecision = truePositives / (truePositives + falsePositives)
+    microRecall = truePositives / (truePositives + falseNegatives)
+    
+    microF1 = ( 2 * microRecall * microPrecision ) / (microPrecision + microRecall) if (microPrecision+microRecall) > 0 else 0
+    # -----------------------------------------------------
+    
+    predictions = predictions.argmax(axis=1)
+    ground = ground.argmax(axis=1)
+    accuracy = np.mean(predictions==ground)
+    
+    print("Accuracy : %.4f, Micro Precision : %.4f, Micro Recall : %.4f, Micro F1 : %.4f" % (accuracy, microPrecision, microRecall, microF1))
+    return accuracy, microPrecision, microRecall, microF1
+
+
+# https://www.kaggle.com/suicaokhoailang/lstm-attention-baseline-0-652-lb
+
+class Attention(Layer):
+    def __init__(self, step_dim,
+                 W_regularizer=None, b_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True, **kwargs):
+        self.supports_masking = True
+        self.init = initializers.get('glorot_uniform')
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        self.step_dim = step_dim
+        self.features_dim = 0
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight((input_shape[-1],),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        self.features_dim = input_shape[-1]
+
+        if self.bias:
+            self.b = self.add_weight((input_shape[1],),
+                                     initializer='zero',
+                                     name='{}_b'.format(self.name),
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+        else:
+            self.b = None
+
+        self.built = True
+
+    def compute_mask(self, input, input_mask=None):
+        return None
+
+    def call(self, x, mask=None):
+        features_dim = self.features_dim
+        step_dim = self.step_dim
+
+        eij = K.reshape(K.dot(K.reshape(x, (-1, features_dim)),
+                        K.reshape(self.W, (features_dim, 1))), (-1, step_dim))
+
+        if self.bias:
+            eij += self.b
+
+        eij = K.tanh(eij)
+
+        a = K.exp(eij)
+
+        if mask is not None:
+            a *= K.cast(mask, K.floatx())
+
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        a = K.expand_dims(a)
+        weighted_input = x * a
+        return K.sum(weighted_input, axis=1)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0],  self.features_dim
+
+
+def microF1Loss(ground, predictions):
+    discretePredictions = K.one_hot(K.argmax(predictions, axis=1), 4)
+    
+    truePositives = K.sum(discretePredictions*ground, axis=0)
+    falsePositives = K.sum(K.clip(discretePredictions - ground, 0, 1), axis=0)
+    falseNegatives = K.sum(K.clip(ground-discretePredictions, 0, 1), axis=0)
+
+    macroPrecision = 0
+    macroRecall = 0
+    for c in range(1, 4):
+        precision = truePositives[c] / (truePositives[c] + falsePositives[c])
+        macroPrecision += precision
+        recall = truePositives[c] / (truePositives[c] + falseNegatives[c])
+        macroRecall += recall
+        f1 = ( 2 * recall * precision ) / (precision + recall + K.epsilon())
+        
+    macroPrecision /= 3
+    macroRecall /= 3
+    macroF1 = (2 * macroRecall * macroPrecision ) / (macroPrecision + macroRecall + K.epsilon())
+    
+    truePositives = K.sum(truePositives[1:])
+    falsePositives = K.sum(falsePositives[1:])
+    falseNegatives = K.sum(falseNegatives[1:])
+    
+    microPrecision = truePositives / (truePositives + falsePositives)
+    microRecall = truePositives / (truePositives + falseNegatives)
+    
+    microF1 = ( 2 * microRecall * microPrecision ) / (microPrecision + microRecall + + K.epsilon())
+    
+    return microF1
+
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    our_string = emoji_pattern.sub(r'', string)
+    needed_string = ""
+    for char in our_string:
+        if not (char in emoji.UNICODE_EMOJI):
+            needed_string += char
+    return needed_string
